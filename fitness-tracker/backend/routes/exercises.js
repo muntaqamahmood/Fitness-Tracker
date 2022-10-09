e18eca4a0274e254108f13b88ca8ef255d18dc7f
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2022-10-09 06:31:33 -0400

    Updated exercises.js

---
+++ fitness-tracker/backend/routes/exercises.js
@@ -0,0 +1,34 @@
+// const exercisesRouter = require('express');
+// const app = exercisesRouter.Router();
+
+const express = require('express');
+const exercisesRouter = express.Router();
+const Exercise = require('../models/Exercise');
+
+// exercisesRouter.get('/', (request, response) => response.send('Hello World'));
+
+exercisesRouter.get('/', (req, res) => {
+    Exercise.find()
+    .then(exercises => res.send(exercises))
+    .catch(error => res.status(400).json('Error: ' + error));
+});
+
+exercisesRouter.post('/add', (req, res) => {
+    const username = req.body.username;
+    const description = req.body.description;
+    const duration = Number(req.body.duration);
+    const date = Date.parse(req.body.date);
+
+    const newExercise = new Exercise({
+        username,
+        description,
+        duration,
+        date,
+    });
+
+    newExercise.save()
+    .then(() => res.send('Exercise added!'))
+    .catch(error => res.status(400).json('Error: ' + error));
+});
+
+module.exports = exercisesRouter;
