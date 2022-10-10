9cba75a101ad0d4812421e384610c67a6919403c
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2022-10-10 03:31:08 -0400

    Updating and adding new CRUD routing for users

--- fitness-tracker/backend/routes/users.js
+++ fitness-tracker/backend/routes/users.js
@@ -1,18 +1,37 @@
-const usersRouter = require('express').Router();
+const express = require('express');
+const usersRouter = express.Router();
 const User = require('../models/User');
 
 usersRouter.get('/', (req, res) => {
     User.find()
-    .then(users => res.json(users))
+    .then(users => res.send(users))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+usersRouter.get('/id', (req, res) => {
+    User.findById(req.params.id)
+    .then(users => res.send(users))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
 usersRouter.post('/add', (req, res) => {
     const username = req.body.username;
-    const newUser = new User(username);
+    const newUser = new User({username});
 
     newUser.save()
-    .then(() => res.json('User Added!'))
+    .then(() => res.send('User Added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+usersRouter.delete('/id', (req, res) => {
+    User.findByIdAndDelete(req.params.id)
+    .then(() => res.send('Deleted user with id: ' + User.id))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+usersRouter.put('/id', (req, res) => {
+    User.findByIdAndUpdate(req.params.id)
+    .then(() => res.send(User.username))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
