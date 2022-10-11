52a5b75781412e7d96402aca67903303134e1e0a
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2022-10-10 22:25:35 -0400

    Fixed bug with duplicated react-router-dom package

--- fitness-tracker/src/App.js
+++ fitness-tracker/src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css"
+// import { Routes } from "react-router-dom";
 
-import Navbar from "./Components/navbar-component";
-import ExercisesList from "./Components/exercise-list-component";
-import EditExercise from "./Components/edit-exercise-component";
-import CreateExercise from "./Components/create-exercise-component";
-import CreateUser from "./Components/create-user-component";
+import Navbar from './Components/navbar-component';
+import ExerciseList from './Components/exercise-list-component';
+import EditExercise from './Components/edit-exercise-component';
+import CreateExercise from './Components/create-exercise-component';
+import CreateUser from './Components/create-user-component';
 
 function App() {
+  // root.render(
   return (
-    <Router>
-      <Navbar />
-      <br />
-      <Route path="/" exact component={ExercisesList} />
-      <Route path="/edit/:id" component={EditExercise} />
-      <Route path="/create" component={CreateExercise} />
-      <Route path="/user" component={CreateUser} />
-    </Router>
+    <BrowserRouter>
+    <Navbar />
+    <br />
+      <Routes>
+        <Route path="/" exact element={<ExerciseList />} />
+        <Route path="/edit/:id" exact element={<EditExercise />} />
+        <Route path="/create" element={<CreateExercise />} />
+        <Route path="/user" element={<CreateUser />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
